50f36b25100381b99e9c1e233c81c45be3765b42
Bartłomiej Klimczak <bartlomiej.klimczak88@gmail.com>
2021-12-23 15:45:55 +0100

    feat: regenerate images

--- generate-featured.sh
+++ generate-featured.sh
@@ -1,26 +1,19 @@
 b64=`cat baseImg.png | base64`
 
+echo "Creating..."
 for dir in content/post/*
 do
     if test -f "$dir/featured.jpg"; then
         continue
     fi
 
-    if test -f "$dir/featured.png"; then
-        continue
-    fi
-
-    echo "$dir doesn't have the featured img. Creating..."
-
-    title=`grep 'title' $dir/index.md | cut -c 8-`
+    title=`grep 'title' $dir/index.md | head -n 1 | cut -c 8-`
     if [[ ${title:0:1} == "\"" ]] ; then 
         title=$(sed 's/.\{1\}$//' <<< "$title")
         title=$(sed 's/^.\{1\}//' <<< "$title")
     fi
 
-    echo $title
-
     curl -X POST -H "Content-Type: application/json" \
     -d "{\"title\": \"$title\", \"baseImg\": \"$b64\"}" \
-    https://imgs.developer20.com/blog-post --output $dir/featured.png
+    https://imgs.developer20.com/blog-post --output $dir/featured.png &> /dev/null
 done
